cc18beeca95a1f4a470bae021f4598d259c9770c
Zewa <zewa666@gmail.com>
2015-01-16 19:31:58 +0100

    removed wrong jspm dependency

--- config.js
+++ config.js
@@ -13,7 +13,6 @@ System.config({
     "angular": "github:angular/bower-angular@1.3.8",
     "angular-mocks": "github:angular/bower-angular-mocks@1.3.8",
     "bootstrap": "github:twbs/bootstrap@3.3.1",
-    "npm:marked": "npm:marked@0.3.2",
     "github:twbs/bootstrap@3.3.1": {
       "css": "github:systemjs/plugin-css@0.1.0",
       "jquery": "github:components/jquery@2.1.3"
