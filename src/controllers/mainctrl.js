a0225ae44099f810cedcde468628d717f4a56e4f
Zewa <zewa666@gmail.com>
2015-06-12 12:38:59 +0200

    * cleanup

--- src/controllers/mainctrl.js
+++ src/controllers/mainctrl.js
@@ -1,13 +1,11 @@
-import {inject, register} from '../decorators/decorators';
+import {register} from '../decorators/decorators';
 
 @register({
   type:'controller',
   inject: ['$scope', 'FlickrService']
 })
-/*@inject('$scope', 'FlickrService')*/
 export class MainCtrl {
   constructor($scope, flickrServiceInstance) {
-    this.images = [];
     this.searchTag = "AngularJS";
     this.service = flickrServiceInstance;
   }
