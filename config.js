8f4adf2fdb960b8171bfb5951f1c48693ba2cb12
Zewa <zewa666@gmail.com>
2015-10-29 09:17:23 +0100

    * version updates

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 System.config({
+  "defaultJSExtensions": true,
   "transpiler": "babel",
   "babelOptions": {
     "optional": [
@@ -8,41 +9,56 @@ System.config({
     ]
   },
   "paths": {
-    "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js",
-    "angular-es6/*": "lib/*.js"
-  },
-  "baseUrl": "dist"
+    "*": "dist/*",
+    "github:*": "jspm_packages/github/*",
+    "npm:*": "jspm_packages/npm/*"
+  }
 });
 
 System.config({
   "map": {
-    "angular": "github:angular/bower-angular@1.4.0",
-    "angular-mocks": "github:angular/bower-angular-mocks@1.4.0",
-    "babel": "npm:babel-core@5.5.7",
-    "babel-runtime": "npm:babel-runtime@5.5.7",
-    "bootstrap": "github:twbs/bootstrap@3.3.4",
-    "core-js": "npm:core-js@0.9.16",
-    "jquery": "github:components/jquery@2.1.3",
-    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
-    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88",
-    "github:angular/bower-angular-mocks@1.4.0": {
-      "angular": "github:angular/bower-angular@1.4.0"
+    "angular": "github:angular/bower-angular@1.4.7",
+    "angular-mocks": "github:angular/bower-angular-mocks@1.4.7",
+    "babel": "npm:babel-core@5.8.32",
+    "babel-runtime": "npm:babel-runtime@5.8.29",
+    "bootstrap": "github:twbs/bootstrap@3.3.5",
+    "core-js": "npm:core-js@0.9.18",
+    "jquery": "github:components/jquery@2.1.4",
+    "github:angular/bower-angular-mocks@1.4.7": {
+      "angular": "github:angular/bower-angular@1.4.7"
     },
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.3.0"
     },
-    "github:twbs/bootstrap@3.3.4": {
-      "jquery": "github:components/jquery@2.1.3"
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.2"
     },
-    "npm:babel-runtime@5.5.7": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
     },
-    "npm:core-js@0.9.16": {
+    "github:twbs/bootstrap@3.3.5": {
+      "jquery": "github:components/jquery@2.1.4"
+    },
+    "npm:assert@1.3.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "npm:babel-runtime@5.8.29": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:core-js@0.9.18": {
       "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.1",
+      "process": "github:jspm/nodelibs-process@0.1.2",
       "systemjs-json": "github:systemjs/plugin-json@0.1.0"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:process@0.11.2": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
     }
   }
 });
